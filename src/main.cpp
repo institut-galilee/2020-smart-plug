dc914a40bd88eb5c10f41e2d742e16924a7693be
AmineYahouni <59349174+AmineYahouni@users.noreply.github.com>
2020-05-20 10:41:18 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,105 @@
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
-
+#include <ezTime.h>
+#include <WiFi.h>
 const char *ssid = "Geny";
 const char *password = "lancer123";
+
+//Variables en lien avec la gestion du temps
+int currentTime[5] = {0, 0, 0, 0, 0}; //Heure = 0, Minute = 1, Jour = 2, Mois = 3, Année = 4
+Timezone myTZ;
+
 
 const int relay = 2;
 const int capteurElectricite = 34;
 
-int valeurDelayOn = 1000;
+int valeurDelayOn;
 bool etatrelay = 0;
-bool etatrelayVoulu = 0;
-int previousMillis = 0;
-int valeurDelayOf =0;
-String valeurdemM;
-String valeurdemH;
-String valeurcoupM;
-String valeurcoupH;
+int valeurDelayOf;
+int valeurdemM;
+int valeurdemH;
+int valeurcoupM;
+int valeurcoupH;
 
 
 AsyncWebServer server(80);
+void getCurrentTime()
+{ 
+  currentTime[0] = myTZ.hour();
+  currentTime[1] = myTZ.minute(); 
+  currentTime[2] = myTZ.day();
+  currentTime[3] = myTZ.month();
+  currentTime[4] = myTZ.year();
+}
+
+int WattTrans(){
+  int i;
+  int pot=0;
+  int cons=0;
+for(i=0;i<9000;i++){
+    for(int j=0;j<1000;j++){
+      cons = cons + analogRead(capteurElectricite);
+    }  
+  cons=cons/1000;
+  pot= pot+cons;
+  }
+  return pot/9000;
+
+}
+
+void timerControl(){
+  bool etatT=1;
+  long int my_timer = millis();
+  int heur =myTZ.hour();
+  int min =myTZ.minute();
+
+  //delay(1000);
+  digitalWrite(relay, HIGH);
+  //delay(1000);
+  while( heur != valeurcoupH || min != valeurcoupM)
+  {
+    Serial.print("while");
+    Serial.print("\n");
+    if(valeurDelayOf != 0 && valeurDelayOn !=0 ){
+      if(etatT)
+      {
+        if ((long)millis() - my_timer > valeurDelayOn*6000 ) {
+        my_timer = millis();   
+         etatT=!etatT;
+        digitalWrite(relay, LOW);
+        }  
+
+      }
+
+      else
+      {
+       if ((long)millis() - my_timer > valeurDelayOf*6000 ) {
+        my_timer = millis();   
+        etatT=!etatT;
+        digitalWrite(relay, HIGH);
+        }
+
+      }
+    }
+    
+    heur = myTZ.hour();;
+    min = myTZ.minute();
+  }
+  digitalWrite(relay, LOW);
+  
+}
+
 
 void setup()
 {
+  
   //----------------------------------------------------Serial
   Serial.begin(115200);
   Serial.println("\n");
 
   //----------------------------------------------------GPIO
   pinMode(relay, OUTPUT);
-  digitalWrite(relay, LOW);
   pinMode(capteurElectricite, INPUT);
 
   //----------------------------------------------------SPIFFS
@@ -53,11 +123,10 @@ void setup()
   //----------------------------------------------------WIFI
   WiFi.begin(ssid, password);
   Serial.print("Tentative de connexion...");
-
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
-    delay(100);
+    delay(10);
   }
 
   Serial.println("\n");
@@ -65,38 +134,44 @@ void setup()
   Serial.print("Adresse IP: ");
   Serial.println(WiFi.localIP());
 
+  waitForSync();
+  
+  myTZ.setLocation(F("fr"));
+
+
+  
+  
+
   //----------------------------------------------------SERVER
-  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/index.html", "text/html");
   });
 
-  server.on("/w3.css", HTTP_POST, [](AsyncWebServerRequest *request) {
+  server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/w3.css", "text/css");
   });
 
-  server.on("/script.js", HTTP_POST, [](AsyncWebServerRequest *request) {
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/script.js", "text/javascript");
   });
 
-  server.on("/jquery-3.5.1.min.js", HTTP_POST, [](AsyncWebServerRequest *request) {
+  server.on("/jquery-3.5.1.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/jquery-3.5.1.min.js", "text/javascript");
   });
 
-  server.on("/lireElectricite", HTTP_POST, [](AsyncWebServerRequest *request) {
-    int val = analogRead(capteurElectricite);
+  server.on("/lireElectricite", HTTP_GET, [](AsyncWebServerRequest *request) {
+    int val = WattTrans();
     String electr = String(val);
     request->send(200, "text/plain", electr);
   });
 
-  server.on("/on", HTTP_POST, [](AsyncWebServerRequest *request) {
-    etatrelayVoulu = 1;
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request) {
+    digitalWrite(relay, HIGH);
     request->send(204);
   });
 
-  server.on("/off", HTTP_POST, [](AsyncWebServerRequest *request) {
-    etatrelayVoulu = 0;
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request) {
     digitalWrite(relay, LOW);
-    etatrelay = 0;
     request->send(204);
   });
 
@@ -106,55 +181,62 @@ void setup()
       String message;
       message = request->getParam("valeurDelayON", true)->value();
       valeurDelayOn = message.toInt();
-    
     }
     if(request->hasParam("valeurDelayOF", true))
     {
       String message;
       message = request->getParam("valeurDelayOF", true)->value();
       valeurDelayOf = message.toInt();
-    
     }
 
 
-     if(request->hasParam("valeurcoupH", true))
+    if(request->hasParam("valeurcoupH", true))
     {
-      valeurcoupH = request->getParam("valeurcoupH", true)->value().toInt();
+      String message;
+      message = request->getParam("valeurcoupH", true)->value();
+      valeurcoupH= message.toInt();
     }
-      if(request->hasParam("valeurcoupM", true))
+    if(request->hasParam("valeurcoupM", true))
     {
-      valeurcoupM = request->getParam("valeurcoupM", true)->value().toInt();
+      String message;
+      message = request->getParam("valeurcoupM", true)->value();
+      valeurcoupM = message.toInt();
+    } 
+    if(request->hasParam("valeurdemH", true))
+    {
+      String message;
+      message= request->getParam("valeurdemH", true)->value();
+      valeurdemH = message.toInt();
+        Serial.println(valeurdemH);
     }
-      
-      if(request->hasParam("valeurdemH", true))
+    if(request->hasParam("valeurdemM", true))
     {
-     valeurdemH = request->getParam("valeurdemH", true)->value().toInt();
+      String message;
+      message = request->getParam("valeurdemM", true)->value();
+      valeurdemM = message.toInt();
+      Serial.println(valeurdemM);
+    }
+
     request->send(204);
-    
-    }
-     if(request->hasParam("valeurdemH", true))
-    {
-     valeurdemM = request->getParam("valeurdemH", true)->value().toInt();
-    request->send(204);
-    }
       
   });
-
+  
   server.begin();
   Serial.println("Serveur actif!");
+  Serial.println(myTZ.dateTime());
+
+
+
+  //setEvent( timerControl , valeurdemH,valeurdemM, 0, myTZ.day(), myTZ.month(), myTZ.year());
+  
+
 }
 
 void loop()
 {
-  if(etatrelayVoulu)
-  {
-    unsigned long currentMillis = millis();
-    if(currentMillis - previousMillis >= valeurDelayOn)
-    {
-      previousMillis = currentMillis;
+    if(valeurdemH == myTZ.hour() && valeurdemM==myTZ.minute()&&myTZ.second()==0 && myTZ.day()&&myTZ.month()&&myTZ.year() ){
 
-      etatrelay = !etatrelay;
-      digitalWrite(relay, etatrelay);
-    }
+    timerControl ();
   }
+  //events();
 }
